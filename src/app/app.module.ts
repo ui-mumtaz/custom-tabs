b61f01f0f84227b7c675dd608701831625dda4e6
mumtaz <mumtazahmadui@gmail.com>
2019-10-06 20:39:28 +0530

    structure of tab pannel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabComponent } from './tab/tab.component';
+import { TabPanelComponent } from './tab-panel/tab-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabComponent,
+    TabPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tab-panel/tab-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tab-panel',
+  templateUrl: './tab-panel.component.html',
+  styleUrls: ['../tab-panel.component.scss']
+})
+export class TabPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
